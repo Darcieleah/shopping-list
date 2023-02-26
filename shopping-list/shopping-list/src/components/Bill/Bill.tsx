1ba0dd72978ea731d9097bcb7a6aa1f515d88b92
Darcieleah <darciehowell@gmail.com>
2023-02-26 00:20:55 +0000

    DH display bill items and total sum

--- shopping-list/shopping-list/src/components/Bill/Bill.tsx
+++ shopping-list/shopping-list/src/components/Bill/Bill.tsx
@@ -1,17 +1,37 @@
-import React, { FC } from 'react';
+import React from 'react';
 import styles from './Bill.module.scss';
-import Product from '../../items/Item';
+import BilledItem from '../../items/BilledItem';
+
 
 type BillProps = {
-  billItems: {id: number}[],
+  billItems: BilledItem[],
+  //TODO: add on add functionality directing from bill
   // onAdd: (product: Product) => void; 
 }
 
+let totalSum = 0;
+
 function Bill({billItems}: BillProps) {
+  // add the prices together for the total sum
+  {for (let i = 0; i < billItems.length; i++) {
+  const billItem = billItems[i];
+  totalSum += billItem.price;}}
+
   return (
   <aside className={styles.Bill} data-testid="Bill">
-     <h2>Your Bill</h2>
-     <div>{billItems.length === 0 && <div>"no items on the bill"</div>}</div>
+    <h2>Your Bill</h2>
+    <div>{billItems.length === 0 && <p>"No items"</p>}</div>
+    {billItems.map((item) => (
+    <div className={styles.billRow} key={item.id}>
+      <p>{item.name}</p>
+      <p>{item.price}</p>
+      <p>{item.quantity}</p>
+    </div>
+    ))}
+    <footer>
+      <p>TOTAL:</p>
+      <p>{totalSum}</p>
+    </footer>
   </aside>
 )};
 

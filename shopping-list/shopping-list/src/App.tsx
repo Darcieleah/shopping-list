8934315dc6ac407e682c52183147dec802e75700
Darcieleah <darciehowell@gmail.com>
2023-02-27 21:25:45 +0000

    DH install es lint and run

--- shopping-list/shopping-list/src/App.tsx
+++ shopping-list/shopping-list/src/App.tsx
@@ -1,32 +1,32 @@
-import React, { useState } from 'react';
-import './App.css';
-import Header from './components/Header/Header';
-import Main from './components/Main/Main';
-import Bill from './components/Bill/Bill';
-import productsData from './product-data';
-import Item from './items/Item';
-import BilledItem from './items/BilledItem';
+import React, { useState } from 'react'
+import './App.css'
+import Header from './components/Header/Header'
+import Main from './components/Main/Main'
+import Bill from './components/Bill/Bill'
+import productsData from './product-data'
+import type Item from './items/Item'
+import type BilledItem from './items/BilledItem'
 
-function App() {
-  const {products} = productsData;
-  const [billItems, setBillItems] = useState<BilledItem[]>([]);
+function App () {
+  const { products } = productsData
+  const [billItems, setBillItems] = useState<BilledItem[]>([])
 
   const onAdd = (product: Item) => {
     // check if the product already exists on the bill
-    const billItem = billItems.find(x => x.id === product.id);
-    if(billItem) {
+    const billItem = billItems.find(x => x.id === product.id)
+    if (billItem != null) {
       // item exists, increase the quantity by 1
-      setBillItems(billItems.map(x => x.id === product.id ? {...billItem, quantity: billItem.quantity +1} : x));
+      setBillItems(billItems.map(x => x.id === product.id ? { ...billItem, quantity: billItem.quantity + 1 } : x))
     } else {
       // item does not exist on the bill yet, add it!
-      setBillItems([...billItems, {...product, quantity:1}])
+      setBillItems([...billItems, { ...product, quantity: 1 }])
     }
-  };
+  }
 
-  //TODO: add remove method which could be used to remove singular items
+  // TODO: add remove method which could be used to remove singular items
 
   const onClear = () => {
-    setBillItems([]);
+    setBillItems([])
   }
 
   return (
@@ -35,7 +35,7 @@ function App() {
       <Main onAdd={onAdd} products={products}></Main>
       <Bill onClear={onClear} billItems={billItems}></Bill>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
